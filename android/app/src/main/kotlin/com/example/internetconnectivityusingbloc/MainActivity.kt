7a355af3ff7a443798b33c44036b686195f6e357
Shetu <mahmudulhasan5008@gmail.com>
2022-12-12 08:05:53 +0600

    internetConnctionusingbloc

---
+++ android/app/src/main/kotlin/com/example/internetconnectivityusingbloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.internetconnectivityusingbloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc/internetbloc/internet_state.dart
@@ -0,0 +1,7 @@
+class InternetState {}
+
+class InternetInitial extends InternetState {}
+
+class InternetLostState extends InternetState {}
+
+class InternetGainedState extends InternetState {}
